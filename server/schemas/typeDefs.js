f196dab9e4c79ce08b63a70929be7f8c910e37fd
Dee <deepakdilse@gmail.com>
2023-12-06 22:58:51 -0800

    update, typedefs, resolvers on server side, also add queries and mutations for client

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,21 @@
 const typeDefs = `
 
 type Book {
-    authors: [String]
-    description: String!
     bookId: String!
+    authors: [String]
+    description: String
     image: String
     link: String
     title: String!
+}
+
+input BookInput {
+    bookId: String!
+    authors: [String]
+    description: String!
+    image: String
+    link: String
+    title: String!  
 }
 
 type User {
@@ -16,11 +25,22 @@ type User {
     bookCount: Int
     savedBooks: [Book]
 }
+type Auth {
+    token: ID!
+    user: User
+}
 
 type Query {
-    user(id: ID!): User
-    users: [User]
+    me: User
 }
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+}
+
 `;
 
 module.exports = typeDefs;
