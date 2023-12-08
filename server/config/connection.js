488531762c40e2c03838673c1b7b3effce8fae76
Dee <deepakdilse@gmail.com>
2023-12-07 22:54:19 -0800

    debug heroku connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
-
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://root:password_123@cluster0.cdag1ph.mongodb.net/googlebooks?retryWrites=true&w=majority');
 module.exports = mongoose.connection;
